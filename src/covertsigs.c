9cba12ef692e50fe8a8f4d8b93266dd3d683daa5
Akazfu <akazfu@gmail.com>
2019-02-08 15:50:44 -0700

    added some comments

--- src/covertsigs.c
+++ src/covertsigs.c
@@ -1,3 +1,13 @@
+/* University of Alberta.
+ * CMPUT379 Assignment 1
+ * -------------------------------------------------------------------------------------------
+ * TOPIC: IPC via Signals
+ * CSID: zfu
+ * Group47, I submitted and completed this assignment alone due to the missing partner issue.
+ * Knowing issue: might crash if stress tested with SINGLE mode,  adjust the TIMESLICE to a higher number if not stable at 5000.
+ */
+
+
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,12 +16,12 @@
 #include <math.h>
 #include <sys/time.h>
 
-#define TIMESLICE 5000
+#define TIMESLICE 5000 //change the TIMESLICE if chasing for higher or lower bits/minute
 
 #ifdef SINGLE
-int SINGLEMODE = 1;
+int SINGLEMODE = 1; //single mode
 #else
-int SINGLEMODE = 0;
+int SINGLEMODE = 0; //double mode
 #endif
 
 int pid2;
@@ -41,7 +51,7 @@ int main()
     sa.sa_flags = 0;
     sa.sa_flags = SA_RESTART;
 
-    printf("Own pid:%d\n", getpid());
+    printf("% covertsigs\nOwn PID:%d\n", getpid());
     scanf("%d", &pid2);
     getchar(); //remove"\n"
 
@@ -66,15 +76,16 @@ void singleMode()
     received_str[0] = '!';
     while (1)
     {
-        if (is_busy)
+        if (is_busy) //is_busy is a checker to prevent interleaving
         {
             while (1)
                 ;
         }
         memset(send_str, 0, 81);
         fgets(send_str, 83, stdin);
-        if (send_str[0] == '.')
+        if (send_str[0] == '.') //Terminate if input '.'
         {
+            printf("%%\n");
             exit(0);
         }
         memset(send_time_array, -1, 81);
@@ -107,6 +118,7 @@ void doubleMode()
         fgets(send_str, 83, stdin);
         if (send_str[0] == '.')
         {
+            printf("%%\n");
             exit(0);
         }
         int i, bit, ch_index;
@@ -119,7 +131,7 @@ void doubleMode()
             c = send_str[ch_index];
             for (i = 7; i >= 0; i--)
             {
-                bit = c & (0x01 << i);
+                bit = c & (0x01 << i); //for every bit of a byte use & to check if its 0 or 1(for the input str)
                 if (kill(pid2, (bit == 0) ? SIGUSR1 : SIGUSR2) == -1)
                 {
                     printf("Error: can't send signal to process %d\n", pid2);
@@ -135,8 +147,8 @@ void singleSigHandler(int signo)
 {
     if (signo == SIGUSR1)
     {
-        //printf("get%d\n",t_count++);
-        last_receive_time = current_receive_time;
+        // printf("get%d\n",t_count++);
+        last_receive_time = current_receive_time; //reset time after every SIGUSR1 Recieved
         current_receive_time = getCurrentUsec();
         if (is_second_signal)
         {
@@ -146,14 +158,14 @@ void singleSigHandler(int signo)
             // printf("current:%ld\n",current_receive_time);
             // printf("interval:%ld\n",interval);
             // printf("round:%lf\n",round((double)(interval) / TIMESLICE));
-            if (round((double)(interval) / TIMESLICE) == 2.0)
+            if (round((double)(interval) / TIMESLICE) == 2.0) //2*TIMESLICE = '0'
             {
                 received_char = received_char << 1;
                 bitcount++;
                 // printf("0");
                 // fflush(stdout);
             }
-            else if (round((double)(interval) / TIMESLICE) == 3.0)
+            else if (round((double)(interval) / TIMESLICE) == 3.0) //3*TIMESLICE = '1'
             {
                 received_char = (received_char << 1) + 1;
                 bitcount++;
@@ -162,8 +174,9 @@ void singleSigHandler(int signo)
             }
             if (bitcount == 8)
             {
-                if ((received_char < 32 || received_char > 127) && received_char != '\n')
-                { //is not ascii and not '\n'
+                // error handling: for every 8 bits if the byte not in range of ASCII and last char is not '\n', return error sign '?'
+                if ((received_char < 32 || received_char > 127) && received_char != '\n') 
+                { // is not ascii and not '\n'
                     received_str[0] = '?';
                 }
                 received_str[received_str_index++] = received_char;
@@ -172,7 +185,7 @@ void singleSigHandler(int signo)
                     int i;
                     for (i = 0; i < received_str_index; i++)
                     {
-                        printf("%c", received_str[i]);
+                        printf("%c", received_str[i]); //print character
                     }
                     memset(received_str, 0, 81);
                     received_str[0] = '!';
@@ -192,12 +205,12 @@ void doubleSigHandler(int signum)
     is_busy = 1;
     if (signum == SIGUSR1)
     {
-        received_char = received_char << 1;
+        received_char = received_char << 1; //SIGUSR1 = '0'
         bitcount++;
     }
     if (signum == SIGUSR2)
     {
-        received_char = (received_char << 1) + 1;
+        received_char = (received_char << 1) + 1; //SIGUSR1 = '1'
         bitcount++;
     }
     if (bitcount == 8)
@@ -229,7 +242,7 @@ void sendSignalSingle()
     int i = 0, j = 0;
     for (i = 0; i < strlen(send_str); i++)
     {
-        for (j = 0; j < 8; j++)
+        for (j = 0; j < 8; j++) //for every bit of every byte
         {
             if (send_time_array[i][j] == -1)
             {
@@ -241,7 +254,7 @@ void sendSignalSingle()
                 printf("Error: can't send signal to process %d\n", pid2);
                 exit(1);
             }
-            usleep(send_time_array[i][j]);
+            usleep(send_time_array[i][j]); //usleep interval of each bit.
             if (kill(pid2, SIGUSR1) == -1)
             {
                 printf("Error: can't send signal to process %d\n", pid2);
@@ -264,11 +277,11 @@ void convertStr2TimeArray()
             int bit = (ch & (0x01 << j)) == 0 ? 0 : 1;
             if (bit)
             {
-                send_time_array[i][7 - j] = TIMESLICE * 3;
+                send_time_array[i][7 - j] = TIMESLICE * 3; //3TIMESLICE if bit = '1'
             }
             else
             {
-                send_time_array[i][7 - j] = TIMESLICE * 2;
+                send_time_array[i][7 - j] = TIMESLICE * 2; //2TIMESLICE if bit = '0'
             }
         }
     }
